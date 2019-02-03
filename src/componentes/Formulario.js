61f2e562d4c9c759e6c59476bc960f1ea1fab9f9
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-03 11:05:44 +0100

    Añadimos funcionalidad a nuestro Formulario y aplicamos el reset

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,18 +1,44 @@
 import React, {Component} from 'react';
 
 class FormularioGasto extends Component {
+
+    // refs para los campos del formulario
+    nombreGastoRef = React.createRef();
+    cantidadGastoRef = React.createRef();
+
+    crearGasto = (e) => {
+
+        //Prevenir el default
+        e.preventDefault();
+
+        // Crear el objeto con los datos
+        const gasto = {
+            nombreGasto: this.nombreGastoRef.current.value,
+            cantidadGasto: this.cantidadGastoRef.current.value
+        };
+
+        console.log(gasto);
+
+        // Agregarlo y enviarlo con props
+
+
+        // Resetear el formulario
+        e.currentTarget.reset();
+
+    };
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.crearGasto}>
                 <h2>Agrega tus gastos aqui</h2>
                 <div className="campo">
                     <label>Nombre Gasto</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. Transporte"/>
+                    <input ref={this.nombreGastoRef} className="u-full-width" type="text" placeholder="Ej. Transporte"/>
                 </div>
 
                 <div className="campo">
                     <label>Cantidad</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. 300"/>
+                    <input ref={this.cantidadGastoRef} className="u-full-width" type="text" placeholder="Ej. 300"/>
                 </div>
 
                 <input className="button-primary u-full-width" type="submit" value="Agregar"/>
