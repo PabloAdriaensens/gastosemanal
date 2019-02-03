a93543f7b18cd36127053ec958c0ed2a23885aea
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-03 12:29:14 +0100

    Creación ControlPresupuesto y conexión a padre App

---
+++ src/componentes/ControlPresupuesto.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ControlPresupuesto extends Component {
+    render() {
+        return (
+            <p>Desde ControlPresupuesto</p>
+        )
+    }
+}
+
+export default ControlPresupuesto;
