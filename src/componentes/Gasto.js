351ed6a8c5054f26635dff89590ff8784964ba3d
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-03 12:13:54 +0100

    Añadimos los gastos en Gasto para llevarlos a Listado y mostrarlos con ciertos estilos ya importados

--- src/componentes/Gasto.js
+++ src/componentes/Gasto.js
@@ -2,8 +2,16 @@ import React, {Component} from 'react';
 
 class Gasto extends Component {
     render() {
+        const {cantidadGasto, nombreGasto} = this.props.gasto;
+        console.log(cantidadGasto);
+        console.log(nombreGasto);
         return (
-            <p>Desde Gasto</p>
+            <li className="gastos">
+                <p>
+                    {nombreGasto}
+                    <span className="gasto"> $ {cantidadGasto} </span>
+                </p>
+            </li>
         )
     }
 }
